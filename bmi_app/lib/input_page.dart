720f4dc91d699252e7ce9c8760114c98e45ea36d
unknown <richardcastuera16@gmail.com>
2023-06-27 15:02:42 +0800

    task(bmi_app): Created files for separate screen

---
+++ bmi_app/lib/input_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  const InputPage({super.key});
+
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text('Body Text'),
+      ),
+    );
+  }
+}
