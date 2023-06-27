0e6f4a66fd8ada58e81e14a0c044df1c7bde007c
unknown <richardcastuera16@gmail.com>
2023-06-27 16:17:14 +0800

    refactor(bmi_app): Added const variable

--- bmi_app/lib/input_page.dart
+++ bmi_app/lib/input_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+const bottomContainerHeight = 80.0;
+const containerColor = Color(0xFF121E33);
+const bottomContainerColor = Color(0xFFEB1555);
+
 class InputPage extends StatefulWidget {
   const InputPage({super.key});
 
@@ -11,53 +15,60 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('BMI CALCULATOR'),
-          centerTitle: true,
-        ),
-        body: Column(
-          children: [
-            Expanded(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Expanded(
-                    child: reusableCard(
-                      Color(0xFF121E33),
-                    ),
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Expanded(
+                  child: reusableCard(
+                    containerColor,
                   ),
-                  Expanded(
-                    child: reusableCard(
-                      Color(0xFF121E33),
-                    ),
+                ),
+                Expanded(
+                  child: reusableCard(
+                    containerColor,
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-            Expanded(
-              child: reusableCard(
-                Color(0xFF121E33),
-              ),
+          ),
+          Expanded(
+            child: reusableCard(
+              containerColor,
             ),
-            Expanded(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Expanded(
-                    child: reusableCard(
-                      Color(0xFF121E33),
-                    ),
+          ),
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Expanded(
+                  child: reusableCard(
+                    containerColor,
                   ),
-                  Expanded(
-                    child: reusableCard(
-                      Color(0xFF121E33),
-                    ),
+                ),
+                Expanded(
+                  child: reusableCard(
+                    containerColor,
                   ),
-                ],
-              ),
-            )
-          ],
-        ));
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: bottomContainerColor,
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: bottomContainerHeight,
+          ),
+        ],
+      ),
+    );
   }
 
   Container reusableCard(Color color) {
