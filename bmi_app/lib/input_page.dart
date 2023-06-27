be155ae8789571d0e1e372cf86cfadc89f02f56d
unknown <richardcastuera16@gmail.com>
2023-06-27 15:52:09 +0800

    refactor(bmi_app): Restructured code by extracting widgets

--- bmi_app/lib/input_page.dart
+++ bmi_app/lib/input_page.dart
@@ -11,12 +11,61 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text('Body Text'),
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Expanded(
+                    child: reusableCard(
+                      Color(0xFF121E33),
+                    ),
+                  ),
+                  Expanded(
+                    child: reusableCard(
+                      Color(0xFF121E33),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: reusableCard(
+                Color(0xFF121E33),
+              ),
+            ),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Expanded(
+                    child: reusableCard(
+                      Color(0xFF121E33),
+                    ),
+                  ),
+                  Expanded(
+                    child: reusableCard(
+                      Color(0xFF121E33),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ));
+  }
+
+  Container reusableCard(Color color) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
